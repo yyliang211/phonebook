cfcc845760dfb28d32560e3bfda23ec38b8e907e
Yingyang Liang <yingyang.liang@tufts.edu>
2024-03-31 17:53:04 -0400

    done 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+morgan.token("person", function getPerson(req, res) {
+  if (req.body.name && req.body.number) {
+    return JSON.stringify(req.body);
+  }
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 let persons = [
   {
