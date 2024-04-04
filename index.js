21ab6708ff9918bcc3b14afc05c2ef89dac11261
Yingyang Liang <yingyang.liang@tufts.edu>
2024-04-03 22:46:15 -0400

    integrate mongodb, add error handling

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
@@ -42,34 +44,32 @@ let persons = [
 ];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.statusMessage = "Person does not exist";
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).send();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-};
-
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", async (req, res) => {
   const body = req.body;
 
   if (body.name == null || body.number == null) {
@@ -78,35 +78,58 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  if (
-    persons.find(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  ) {
+  const existingPerson = await Person.findOne({ name: body.name });
+
+  if (existingPerson) {
     return res.status(422).json({
       error: "name must be unique",
     });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 100000),
-  };
+  });
 
-  persons = persons.concat(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
+});
 
-  res.json(person);
+app.put("/api/persons/:id", (req, res, next) => {
+  const { name, number } = req.body;
+
+  const person = { name, number };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
-  const numPersons = persons.length;
   const date = new Date(Date.now());
   const dateString = date.toUTCString();
-  res.send(`Phonebook has info for ${numPersons} people<br />${date}`);
+  Person.countDocuments({})
+    .exec()
+    .then((count) => {
+      res.send(`Phonebook has info for ${count} people<br />${date}`);
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(404).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
